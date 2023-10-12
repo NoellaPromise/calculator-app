4663bb9bf31ebbdea93d9504c77b4257721525bd
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-10-12 17:10:12 +0200

    Add design

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className=" flex justify-center  items-center">
+      <div className="h-[60vh] bg-orange-200 rounded-md m-4 ">
+        <form className="font-bold  m-4 p-2  text-xl  ">
+          <div>
+            <input type="text" className="p-2 mb-3" />
+          </div>
+          <div >
+            <input type="button" value="AC"className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 "  />
+            <input type="button" value="DE"className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 " />
+            <input type="button" value="." className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 "/>
+            <input type="button" value="/" className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 "/>
+          </div>
+          <div >
+            <input type="button" value="7"className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 " />
+            <input type="button" value="8"className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 " />
+            <input type="button" value="9"className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 " />
+            <input type="button" value="*"className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 " />
+          </div>
+          <div >
+            <input type="button" value="4" className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 "/>
+            <input type="button" value="5" className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 " />
+            <input type="button" value="6"className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 " />
+            <input type="button" value="+"className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 " />
+          </div>
+          <div >
+            <input type="button" value="1" className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 "/>
+            <input type="button" value="2" className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 "/>
+            <input type="button" value="3" className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 "/>
+            <input type="button" value="-" className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 "/>
+          </div>
+          <div >
+            <input type="button" value="00"className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 " />
+            <input type="button" value="0" className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 "/>
+            <input type="button" value="=" className="  bg-blue-400  border-black rounded-sm m-2 border-none outline-0 w-12 h-12 "/>
+          </div>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
